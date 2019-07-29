b5d10cec6d37c5aa06b3f6838ab144cad4cc7fd5
kirsi-k <kirsikujala@hotmail.com>
2019-07-29 14:35:51 +0300

    fronttikansiossa koodit siistimmin

---
+++ src/main/frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TehtavaBox from "./components/TehtavaBox";
+
+function App() {
+  return (
+      <div className="App">
+        <TehtavaBox/>
+      </div>
+  );
+}
+
+export default App;
